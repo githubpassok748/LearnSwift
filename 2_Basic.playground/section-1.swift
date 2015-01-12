374a1f34068603be76a7daec8c65a2eceadb55b5
foryoo <foryoo@foryoodeiMac.local>
2015-01-12 15:38:40 +0800

    add Swift 2 part

---
+++ 2_Basic.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+//Int、Double、Float、Bool、String，集合类型：Array和Dictionary
+/*Swift增加：
+*1、元组（Tuple）：创建和传递一组数据
+*2、可选类型（Optional：处理值缺失情况
+*/
+
+//常量和变量
+let maxinumNumberOfLoginAttepts = 10
+var currentLoginAttempt = 0
+
+//在一行中声明多个常量或变量
+
