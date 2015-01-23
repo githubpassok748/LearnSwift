04ddf0569401347c0d209604762cfb1bc2dbcd8d
foryoo <foryoo@foryoodeiMac.local>
2015-01-23 17:22:25 +0800

    add Swift lession 15 for 2 part and start video lession from 51cto

--- 3_video_lession.playground/section-1.swift
+++ 3_video_lession.playground/section-1.swift
@@ -223,4 +223,185 @@ import Foundation
 //limitedCar.speed = 60.0
 //println("SpeedLitimedCar: \(limitedCar.description())")
 
+//
+////默认构造器:构造函数无返回值
+//struct Fahrenhelt {
+//    var temperature: Double
+////    init() {
+////        temperature = 32.0
+////    }
+//}
+//var f = Fahrenhelt(temperature: 32.0)
+//println("The default temperature is \(f.temperature) Fahrentheit")
 
+////自定义构造器
+//struct Celsius {
+//    var temperatureInCelsius: Double = 0.0
+//    
+//    init(fromFahrenheit fahrenheit: Double, aacc aaa: Int) {
+//        temperatureInCelsius = (fahrenheit - 32.0 / 1.0)
+//    }
+//    init(fromKelvin kelvin: Double) {
+//        temperatureInCelsius = kelvin - 273.15
+//    }
+//}
+//let boilingPointOfWater = Celsius(fromFahrenheit: 212.0, aacc: 1)
+//let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+//
+////析构器: 在可选类型对象值设为nil时调用deinit()
+//class Player {
+//    var coinsInPurse: Int
+//    
+//    init(coins: Int) {
+//        println("call init")
+//        coinsInPurse = coins
+//    }
+//    func winCoins(coins: Int) {
+//        coinsInPurse += 10
+//    }
+//    deinit {
+//        coinsInPurse = 0
+//    }
+//}
+//
+//var playerOne: Player? = Player(coins: 100)
+//println("coinsInPurse: \(playerOne!.coinsInPurse) coins")
+//
+//playerOne = nil
+//
+//println("PlayerOne has left the game")
+
+////扩展、计算属性(extension)
+//extension Double {
+//    var km: Double { return self * 1_000.0 }
+//    var m: Double { return self }
+//    var cm: Double { return self/100.0 }
+//    var mm: Double { return self / 1_000.0 }
+//    var ft: Double { return self / 3.28084 }
+//    
+//}
+//let oneInch = 25.4.mm
+//println("One inch is \(oneInch) meters")
+//let threeFeet = 3.ft
+//println("Three feet is \(threeFeet) meters")
+//
+//let aMarathon = 42.km + 195.m
+//println("A marathon is \(aMarathon) meters long")
+
+////扩展使用构造器
+//struct Size {
+//    var width = 0.0, height = 0.0
+//}
+//struct Point {
+//    var x = 0.0, y = 0.0
+//}
+//struct Rect {
+//    var origin = Point()
+//    var size = Size()
+//}
+//
+//extension Rect {
+//    init(center: Point, size: Size) {
+//        let originX = center.x - (size.width / 2)
+//        let originY = center.y - (size.height / 2)
+//        self.init(origin: Point(x: originX, y: originY), size: size)
+//    }
+//}
+//
+//let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
+
+////方法扩展
+//extension Int {
+//    func message() -> String {
+//        var message = "";
+//        switch self {
+//        case 0:
+//            message = "成功"
+//        case -1:
+//            message = "用户登录失败"
+//        default:
+//            message = "未知错误"
+//        }
+//        return message
+//    }
+//}
+//
+//3.message()
+//0.message()
+//(-1).message()
+
+////协议: 定义方法和属性
+//protocol FullyNamed {
+//    var fullName: String { get }
+//}
+//
+//struct Person: FullyNamed {
+//    var fullName: String
+//}
+//let john = Person(fullName: "john Appleseed")
+//
+//class Starship: FullyNamed {
+//    var prefix: String?
+//    var name: String
+//    init (name: String, prefix: String? = nil) {
+//        self.name = name
+//        self.prefix = prefix
+//    }
+//    var fullName: String {
+//        return ( prefix != nil ? prefix! + " " : "") + name
+//    }
+//}
+//
+//var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+
+////协议中使用方法
+//protocol RandomNumberGenerator {
+//    func random() -> Double
+//}
+//
+//class LinearCongruentialGenerator: RandomNumberGenerator {
+//    var lastRandom = 42.0
+//    let m = 139968.0
+//    let a = 3877.0
+//    let c = 29573.0
+//    func random() -> Double {
+//        lastRandom = (( lastRandom * a + c) % m )
+//        return lastRandom / m
+//    }
+//}
+//let generator = LinearCongruentialGenerator ()
+//println("Here's a random number: \(generator.random())")
+//println("And another one: \(generator.random())")
+
+//把协议作为类型使用
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = (( lastRandom * a + c) % m )
+        return lastRandom / m
+    }
+}
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init (sides: Int, generator: RandomNumberGenerator ) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
+for _ in 1...5 {
+    println("Random dice roll is \( d6.roll())")
+}
