e211965654eb816a223697a5fd2d325a117f7909
foryoo <foryoo@foryoodeiMac.local>
2015-01-30 16:31:17 +0800

    add Swift lesssion 19 for SwiftUI Demo instance-02 vacation beta

---
+++ vaction/vaction/AddVacationViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AddVacationViewController.swift
+//  vaction
+//
+//  Created by foryoo on 15/1/30.
+//  Copyright (c) 2015年 foryoo. All rights reserved.
+//
+
+import UIKit
+
+class AddVacationViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    
+    var vacation = Vacation()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        if sender as NSObject == doneButton {
+            if (!textField.text.isEmpty) {
+                vacation.place = textField.text
+            }
+        }
+        
+    }
+    
+
+}
---
+++ vaction/vaction/Vacation.swift
@@ -0,0 +1,15 @@
+//
+//  Vacation.swift
+//  vaction
+//
+//  Created by foryoo on 15/1/30.
+//  Copyright (c) 2015年 foryoo. All rights reserved.
+//
+
+import Foundation
+
+class Vacation {
+    var place = ""
+    var visited = false
+    
+}
